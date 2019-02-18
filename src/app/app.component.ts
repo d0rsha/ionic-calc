7e5c42acda1b58c4c4576253fcd17f034d3f8619
Anders@Klumpisen <avrethem@gmail.com>
2019-02-18 19:20:22 +0100

    Calc init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { timer } from 'rxjs/observable/timer';
 
 @Component({
   selector: 'app-root',
@@ -21,6 +22,8 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      timer(500).subscribe(() => this.splashScreen.hide());
     });
   }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs/observable/timer';
+import { AppComponent } from '../app.component';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
+  display = '';
+  loadScreen = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClick(button) {
+
+    if (button === 'C') {
+      this.display = '';
+    } else if (button === '=') {
+
+
+      const megaSpeed = document.getElementById('super-speed');
+      console.log(megaSpeed['checked']);
+      if (!megaSpeed['checked']) {
+        this.loadScreen = true;
+        timer(800).subscribe(() => this.loadScreen = false);
+      }
+      this.display = eval(this.display);
+    } else {
+      this.display += button;
+    }
+  }
 }
